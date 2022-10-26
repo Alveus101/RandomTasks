8f29dd04ffa6acc1b42cd8ee4c3d2513240bcaf4
Alveus <84937460+Alveus101@users.noreply.github.com>
2022-10-26 14:34:09 +0200

    Create Min&Max.c

---
+++ Min&Max.c
@@ -0,0 +1,31 @@
+/*
+Napiši program koji će učitati N brojeva i ispisati najmanji i najveći broj, te na kojem se mjestu unosa nalaze.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int kolicina, broj, min = 0, max = 0, minPlace, maxPlace;
+
+    scanf("%d", &kolicina);
+
+    for (int i = 1; i <= kolicina; i++) {
+        scanf("%d", &broj );
+        
+        if (broj > max) {
+            max = broj;
+            maxPlace = i;
+        }
+        if (broj < min) {
+            min = broj;
+            minPlace = i;
+        }
+
+    }
+
+    printf("Minimum: %d na %d. mjestu.\nMaximum: %d na %d. mjestu.", min, minPlace, max, maxPlace);
+
+    return 0;
+}
