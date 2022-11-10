49388b7c654223563469c358eaceeb51f6069b57
Alveus <84937460+Alveus101@users.noreply.github.com>
2022-11-10 22:53:04 +0100

    Create ZV44 - String reverse.c

---
+++ ZV44 - String reverse.c
@@ -0,0 +1,34 @@
+/*
+Napiši program koji unosi maksimalno 10 znakovnih nizova maksimalne duljine
+100 znakova. Program treba ispisati nizove u obrnutom redoslijedu znakova.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_STRINGOVA 3
+#define MAX_ZNAKOVA 100
+#include <stdio.h>
+
+int main()
+{
+	char string[MAX_STRINGOVA][MAX_ZNAKOVA];
+	int charCount[MAX_STRINGOVA]{};
+
+	for (int i = 0; i < MAX_STRINGOVA; i++) {
+		
+		gets_s(string[i]);
+		
+		for (int j = 0; j < MAX_ZNAKOVA; j++) {
+			if ((int)string[i][j] >= 0 && (int)string[i][j] <= 127)
+				charCount[i]++;
+		}
+	}
+		
+	for (int i = 0; i < MAX_STRINGOVA; i++) {
+		for (int j = charCount[i]-1; j >= 0; j--) {
+			printf("%c", string[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
